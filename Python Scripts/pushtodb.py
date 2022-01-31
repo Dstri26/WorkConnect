cb3ac7b4b9c4dfafb5fe70fb547a3058db4d73cc
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2022-01-31 21:36:35 +0530

    Update pushtodb.py

--- Python Scripts/pushtodb.py
+++ Python Scripts/pushtodb.py
@@ -1,15 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
+from getmails import data_frame
 
-df = pd.read_csv('dataset.csv')
-
-#password - ashishkumarakp
-#table_name - workconnect
-#db_name - retrieved_info
-#username - root
-#host - localhost
+#df = pd.read_csv('Final_dataset.csv')
 
 my_conn=create_engine("mysql+mysqldb://root:ashishkumarakp@localhost/workconnect")
-df.to_sql(con=my_conn,name='retrieved_info',if_exists='replace')
+data_frame.to_sql(con=my_conn,name='retrieved_info',if_exists='replace')
 print("Uploaded to database")
